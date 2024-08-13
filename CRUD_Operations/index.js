f5ef49a36fa1273bf71e41aa83b3aed403e70b97
Shrinivas <shrinivasbakale2002@gmail.com>
2024-08-13 23:34:45 +0530

    Initial commit

---
+++ CRUD_Operations/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Hello world");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from node API");
+});
