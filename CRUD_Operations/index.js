bd4bde569a1d853f4b20fc071f576c38485f95b1
Shrinivas <shrinivasbakale2002@gmail.com>
2024-08-14 11:59:44 +0530

    All the crud operations

--- CRUD_Operations/index.js
+++ CRUD_Operations/index.js
@@ -1,13 +1,72 @@
 const express = require("express");
 const app = express();
+const Product = require("./models/product.model.js");
 const mongoose = require("mongoose");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from node API Helloo");
 });
 
-app.post("/api/products", (req, res) => {
-  res.send("Data Received");
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({message:"Product Deleted successfully"});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
@@ -16,9 +75,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to Database!");
-    app.listen(3000, () => {
-      console.log("Hello world");
-    });
+    app.listen(3000, () => {});
   })
   .catch(() => {
     console.log("Connection Failed!");
